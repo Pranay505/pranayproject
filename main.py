cf91c22c9bf87b7c3c5d7dcad942b616e1c1a4a8
Pranay505 <sadh.pranay@gmail.com>
2022-03-03 00:39:38 -0500

    main updated

--- main.py
+++ main.py
@@ -34,6 +34,8 @@ class Car:
     def __init__(self, max_speed: int):
         self.max_speed = max_speed
         self.speed=0
+        
+        
     def acc(self, speed:int):
         self.speed = self.speed+speed
 
